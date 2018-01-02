9d8f5277fe7c408fc1abda81010a447bcb8bc1a3
tan <tanmaykm@gmail.com>
2018-01-02 12:46:03 +0530

    use a more recent version of etcd

--- src/constants.jl
+++ src/constants.jl
@@ -1,22 +1,23 @@
+global const ETCD_VER = "v3.2.12"
 global const ETCD_DEPS_PATH = joinpath(dirname(dirname(@__FILE__)), "deps")
 global const ETCD_SRC_PATH = if is_apple()
-    joinpath(ETCD_DEPS_PATH, "etcd-v2.3.7-darwin-amd64.zip")
+    joinpath(ETCD_DEPS_PATH, "etcd-$(ETCD_VER)-darwin-amd64.zip")
 elseif is_linux()
-    joinpath(ETCD_DEPS_PATH, "etcd-v2.3.7-linux-amd64.tar.gz")
+    joinpath(ETCD_DEPS_PATH, "etcd-$(ETCD_VER)-linux-amd64.tar.gz")
 end
 
 global const ETCD_DEST_PATH = if is_apple()
-    joinpath(ETCD_DEPS_PATH, "etcd-v2.3.7-darwin-amd64")
+    joinpath(ETCD_DEPS_PATH, "etcd-$(ETCD_VER)-darwin-amd64")
 elseif is_linux()
-    joinpath(ETCD_DEPS_PATH, "etcd-v2.3.7-linux-amd64")
+    joinpath(ETCD_DEPS_PATH, "etcd-$(ETCD_VER)-linux-amd64")
 end
 
 global const ETCD_BIN = joinpath(ETCD_DEST_PATH, "etcd")
 
 global const ETCD_DOWNLOAD_URI = if is_apple()
-    "https://github.com/coreos/etcd/releases/download/v2.3.7/etcd-v2.3.7-darwin-amd64.zip"
+    "https://github.com/coreos/etcd/releases/download/$(ETCD_VER)/etcd-$(ETCD_VER)-darwin-amd64.zip"
 elseif is_linux()
-    "https://github.com/coreos/etcd/releases/download/v2.3.7/etcd-v2.3.7-linux-amd64.tar.gz"
+    "https://github.com/coreos/etcd/releases/download/$(ETCD_VER)/etcd-$(ETCD_VER)-linux-amd64.tar.gz"
 else
     ""
 end
